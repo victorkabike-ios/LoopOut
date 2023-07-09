58f6be3cb930dc3ebe1f8f8d122bf761e93da216
Victor Kabike <tinotendakab@gmaail.com>
2023-07-09 20:23:40 +0200

    Initial Commit

---
+++ LoopOut/LoopOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopOutApp.swift
+//  LoopOut
+//
+//  Created by victor kabike on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct LoopOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
